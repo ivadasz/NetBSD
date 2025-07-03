00054638e625720f4989dfa1278a66978aec848a
Imre Vadász <imrevdsz@gmail.com>
2025-07-03 20:01:59 +0200

    Add hack to enable ITE HDMI driver without device-tree entry, via fixed kernel config line

--- sys/dev/i2c/i2c.c
+++ sys/dev/i2c/i2c.c
@@ -531,6 +531,7 @@ iic_attach(device_t parent, device_t self, void *aux)
 			if (buf)
 				free(buf, M_TEMP);
 		}
+		iic_rescan(self, "iic", NULL);
 	} else if (!no_indirect_config) {
 		/*
 		 * Attach all i2c devices described in the kernel
